4af0bfc2e2966c46c5505c2176e8838e546b454b
まっぴー <mahirotakahashinoakaunto@gmail.com>
2026-02-14 02:19:26 +0900

    Add files via upload
    Todo追加機能を実装（メモリ保存）

--- ToDo/__init__.py
+++ ToDo/__init__.py
@@ -1,14 +1,21 @@
 # FlaskからFlaskというクラスをインポート
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 
 # Flaskアプリのインスタンスを作成（クラスがたいやきの型ならインスタンスはたいやき）　ちなみに俺はたいやき割と好き（？） 
 app = Flask(__name__)
 
+# todosというリストを作成
+todos = []
+
 # rootを定義して(/)にアクセスがあったら以下の関数を呼び出す。
 @app.route('/')
 def index():
-    todos = [
-        "sample task 1",
-        "sample task 2",
-    ]
+    # todos = []を削除
     return render_template("index.html", todos=todos)
+
+@app.route("/add", methods=["POST"])
+def add():
+    todo = request.form.get("todo")
+    if todo:
+        todos.append(todo)
+    return redirect("/")
